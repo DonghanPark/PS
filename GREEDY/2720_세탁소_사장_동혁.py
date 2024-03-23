aa904aba930a0fe55fa05fc477e53bb9d46128d4
DonghanPark <qkrehdgks11@naver.com>
2024-03-23 22:42:31 +0900

    2720 solved

---
+++ GREEDY/2720_세탁소_사장_동혁.py
@@ -0,0 +1,14 @@
+def main():
+    tc = int(input())
+    for _ in range(tc):
+        change = int(input())
+        answer = []
+        
+        for j in [25, 10, 5, 1]:
+            answer.append(change//j)
+            change = change%j
+            
+        print(*answer)
+
+if __name__ == '__main__':
+    main()
