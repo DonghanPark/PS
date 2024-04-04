9490bf1d9fee865792f55ebb6f75edb6d86beca4
DonghanPark <qkrehdgks11@naver.com>
2024-04-04 21:54:41 +0900

    1026 solved

--- GREEDY/1026_보물.py
+++ GREEDY/1026_보물.py
@@ -1,4 +1,15 @@
+# 24.04.04
+
 def main():
+    len = int(input())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    A.sort()
+    B.sort(reverse=True)
+    sum = 0
+    for i in range(len):
+        sum = sum + (A[i]*B[i])
+    print(sum)
     return 0     
 
 if __name__ == "__main__":
