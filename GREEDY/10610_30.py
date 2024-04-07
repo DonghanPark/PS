104252e75060d9fb87e78b2324388fd23efed888
DonghanPark <qkrehdgks11@naver.com>
2024-04-07 21:54:44 +0900

    10610 solved

--- GREEDY/10610_30.py
+++ GREEDY/10610_30.py
@@ -1,4 +1,13 @@
+# 24.04.07
+# A better solution exists
+
 def main():
+    n = input()
+    max_num = int(''.join(sorted(n, reverse=True)))
+    if (max_num%30 == 0):
+        print(max_num)
+    else:
+        print(-1)
     return 0     
 
 if __name__ == "__main__":
