2979bee2a8c72cdad6fbeea0530c791599d0eee2
DonghanPark <qkrehdgks11@naver.com>
2024-04-08 21:04:40 +0900

    1049 solved

--- GREEDY/1049_기타줄.py
+++ GREEDY/1049_기타줄.py
@@ -1,4 +1,20 @@
+import sys
+
 def main():
+    m, n = map(int, sys.stdin.readline().split())
+    price = 0
+    six_price = 1000
+    one_price = 1000
+    for _ in range(n):
+        six, one = map(int, sys.stdin.readline().split())
+        six_price = min(six_price, six)
+        one_price = min(one_price, one)
+    if m >= 6:
+        price += min(six_price, one_price*6)*(m//6)
+        m %= 6
+    if m > 0:
+        price += min(six_price, one_price*m)
+    print(price)
     return 0
 
 if __name__ == "__main__":
