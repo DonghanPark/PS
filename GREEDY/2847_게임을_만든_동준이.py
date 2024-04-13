6212b712cd8f221cf747d573297ccf36f1d4732f
DonghanPark <qkrehdgks11@naver.com>
2024-04-13 22:37:03 +0900

    2847 solved

--- GREEDY/2847_게임을_만든_동준이.py
+++ GREEDY/2847_게임을_만든_동준이.py
@@ -1,4 +1,17 @@
+# 24.04.13
+# 끝에 한 틱 다르게 줄 수 있음
+
 def main():
+    N = int(input())
+    num = []
+    asw = 0
+    for _ in range(N):
+        num.append(int(input()))
+    for j in range(N-2, -1, -1):
+        if num[j] >= num[j+1]:
+           asw += (num[j] - num[j+1] +1)
+           num[j] -=  (num[j] - num[j+1] +1)
+    print(asw)
     return 0
 
 if __name__ == "__main__":
