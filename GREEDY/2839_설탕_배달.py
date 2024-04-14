264e06103e82ca6cc682ffd55ffaf7b5d419f4f9
DonghanPark <qkrehdgks11@naver.com>
2024-04-14 20:15:27 +0900

    2839 solved

--- GREEDY/2839_설탕_배달.py
+++ GREEDY/2839_설탕_배달.py
@@ -1,4 +1,22 @@
+# 24.04.14
+# 좀 더 생각하기
+# 접근은 발전했음
+
 def main():
+    N = int(input())
+    asw = 0
+    if N%5 == 0:
+        print(N//5)
+        return 0
+    else:
+        while N >= 3:
+            N -= 3
+            asw += 1
+            if N%5 == 0:
+                print(asw + N//5)
+                return 0
+    if N != 0:
+        print(-1)
     return 0
 
 if __name__ == '__main__':
