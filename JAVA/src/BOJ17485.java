34877fab47e1b5061a678cd3484957daa40debcf
DonghanPark <qkrehdgks11@naver.com>
2025-08-30 20:41:44 +0900

    solved BOJ17485 진우의 달 여행 (Large)

--- JAVA/src/BOJ17485.java
+++ JAVA/src/BOJ17485.java
@@ -4,10 +4,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ17485 {
-    private static boolean inRange(int x, int y, int N, int M) {
-        return (x >= 0 && x < N && y >= 0 && y < M);
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -15,10 +11,10 @@ public class BOJ17485 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] map = new int[N][M];
-        for (int i = 0; i < N; i++) {
+        int[][] map = new int[N + 2][M + 2];
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
+            for (int j = 1; j <= M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
@@ -27,37 +23,33 @@ public class BOJ17485 {
         int[] dx = {-1, -1, -1};
         int[] dy = {0, 1, -1};
 
-        int[][] dp = new int[N][M];
-        int[][] prevDir = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                int minFuel = Integer.MAX_VALUE;
-                int minFuelDir = -1;
-                for (int dir = 0; dir < 3; dir++) {
-                    int nx = i + dx[dir];
-                    int ny = j + dy[dir];
-
-                    if (!inRange(nx, ny, N, M)) continue;
-                    if (prevDir[nx][ny] == dir) continue;
-
-                    if (dp[nx][ny] < minFuel) {
-                        minFuel = dp[nx][ny];
-                        minFuelDir = dir;
-                    }
-                }
-
-                if (minFuel == Integer.MAX_VALUE)
-                    minFuel = 0;
-
-                dp[i][j] = map[i][j] + minFuel;
-                prevDir[i][j] = minFuelDir;
+        int[][][] dp = new int[N + 2][M + 2][3];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < 3; j++) {
+                dp[i][0][j] = 1_000_000;
+                dp[i][M + 1][j] = 1_000_000;
+            }
+        }
+        for (int i = 1; i <= M; i++) {
+            for (int j = 0; j < 3; j++) {
+                dp[1][i][j] = map[1][i];
             }
         }
 
-        int finalMinFuel = Integer.MAX_VALUE;
-        for (int i = 0; i < M; i++) {
-            finalMinFuel = Math.min(finalMinFuel, dp[N - 1][i]);
+        for (int i = 2; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                dp[i][j][0] = map[i][j] + Math.min(dp[i + dx[0]][j + dy[0]][1], dp[i + dx[0]][j + dy[0]][2]);
+                dp[i][j][1] = map[i][j] + Math.min(dp[i + dx[1]][j + dy[1]][0], dp[i + dx[1]][j + dy[1]][2]);
+                dp[i][j][2] = map[i][j] + Math.min(dp[i + dx[2]][j + dy[2]][0], dp[i + dx[2]][j + dy[2]][1]);
+            }
         }
-        System.out.println(finalMinFuel);
+
+        int minFuel = Integer.MAX_VALUE;
+        for (int i = 1; i <= M; i++) {
+            for (int j = 0; j < 3; j++) {
+                minFuel = Math.min(minFuel, dp[N][i][j]);
+            }
+        }
+        System.out.println(minFuel);
     }
 }
